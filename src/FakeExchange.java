d0c90fbe3e5cf328701a71727abe488063051fbf
Beth <shiehbeth@gmail.com>
2025-02-26 20:46:30 -0800

    updated constant naming for fake exchange

--- src/FakeExchange.java
+++ src/FakeExchange.java
@@ -1,13 +1,13 @@
 public class FakeExchange implements Exchange {
 
-  private final static float usdToEuroRate = 0.95f;
+  private final static float USD_TO_EURO_RATE = 0.95f;
 
   public FakeExchange() {
   }
 
   @Override
   public float rate(String origin, String target) {
-      return usdToEuroRate;
+      return USD_TO_EURO_RATE;
   }
 
 }
